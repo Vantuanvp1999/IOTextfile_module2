8b62fc6f3b0c3065fd36b593f96f800bd9c0542f
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-25 09:34:01 +0700

    add

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nhập đường dẫn file");
-        Scanner sc = new Scanner(System.in);
-        String path = sc.nextLine();
-        ReadFileExample readFileExample = new ReadFileExample();
-        readFileExample.readFileText(path);
+
     }
 
 }
